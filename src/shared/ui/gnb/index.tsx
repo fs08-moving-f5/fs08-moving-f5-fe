4cc9f50ca6b829d481484c91ef205c34dcb44537
MinJun916 <ed125248@gmail.com>
2025-12-08 17:22:28 +0900

    feat: add responsive design to GNB component

--- src/shared/ui/gnb/index.tsx
+++ src/shared/ui/gnb/index.tsx
@@ -47,12 +47,18 @@ const GNB = () => {
   };
 
   return (
-    <div className="flex h-[88px] w-full items-center justify-between bg-white px-[160px] py-[26px]">
+    <div className="mobile:h-[54px] mobile:py-[10px] tab:py-[19px] tab:px-[72px] mobile:px-[24px] tab:h-]54px] flex h-[88px] w-full items-center justify-between bg-white px-[160px] py-[26px]">
       <div className="flex items-center gap-[82px]">
         <Link href="/">
-          <Image src="img/logo-text.svg" alt="logo" width={116} height={44} />
+          <Image
+            src="img/logo-text.svg"
+            alt="logo"
+            width={116}
+            height={44}
+            className="mobile:w-[88px] tab:w-[88px] mobile:h-[34px] tab:h-[34px]"
+          />
         </Link>
-        <div className="flex items-center gap-10">
+        <div className="mobile:hidden tab:hidden flex items-center gap-10">
           {menuByRole[user.role].map((menu) => (
             <Link key={menu.id} href={menu.href}>
               <div className="text-black-500 text-2lg font-bold">{menu.label}</div>
@@ -60,16 +66,38 @@ const GNB = () => {
           ))}
         </div>
       </div>
-      {user.role === 'guest' && <button type="button">로그인</button>}
-      {user.role !== 'guest' && (
-        <div className="flex items-center gap-8">
-          <Image src="icons/alarm.svg" alt="alarm" width={36} height={36} />
-          <div className="flex items-center gap-4">
-            <Image src="icons/profile.svg" alt="profile" width={36} height={36} />
-            <div className="text-black-500 text-2lg font-medium">{user.name}</div>
+      <div className="mobile:hidden tab:hidden">
+        {user.role === 'guest' && <button type="button">로그인</button>}
+        {user.role !== 'guest' && (
+          <div className="flex items-center gap-8">
+            <Image src="icons/alarm.svg" alt="alarm" width={36} height={36} />
+            <div className="flex items-center gap-4">
+              <Image src="icons/profile.svg" alt="profile" width={36} height={36} />
+              <div className="text-black-500 text-2lg font-medium">{user.name}</div>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
+      <div className="mobile:flex tab:flex hidden items-center">
+        {user.role === 'guest' && (
+          <button type="button">
+            <Image src="icons/menu.svg" alt="menu" width={24} height={24} />
+          </button>
+        )}
+        {user.role !== 'guest' && (
+          <div className="flex items-center gap-6">
+            <button type="button">
+              <Image src="icons/alarm.svg" alt="alarm" width={24} height={24} />
+            </button>
+            <button type="button">
+              <Image src="icons/profile.svg" alt="profile" width={24} height={24} />
+            </button>
+            <button type="button">
+              <Image src="icons/menu.svg" alt="menu" width={24} height={24} />
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
