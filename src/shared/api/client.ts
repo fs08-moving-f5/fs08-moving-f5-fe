b76a5e5ac1f41b1dbb7ffb7d65233b34e2207f6d
kwangche <dlrhkdcp11@gmail.com>
2025-12-19 11:45:09 +0900

    feat: apiClient에 쿠키 포함 설정 추가

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -10,6 +10,7 @@ import {
 const apiClient = ky.create({
   prefixUrl: process.env.NEXT_PUBLIC_API_URL,
   timeout: 30 * 1000, // 응답대기 30초
+  credentials: 'include', // 쿠키 포함
   headers: {
     'Content-Type': 'application/json',
   },
