6fd419e3b4b947c1d508c908132ee828ad430402
MinJun916 <ed125248@gmail.com>
2025-12-19 10:25:51 +0900

    feat: build my-estimates tab UI

--- src/app/user/my/estimates/pending/page.tsx
+++ src/app/user/my/estimates/pending/page.tsx
@@ -1,5 +1,13 @@
+import GNB from '@/shared/ui/gnb';
+import PendingEstimatesTab from '@/features/my-estimates/pending/ui/tab';
+
 const PendingEstimatesPage = () => {
-  return <div>PendingEstimatesPage</div>;
+  return (
+    <div>
+      <GNB />
+      <PendingEstimatesTab activeTab="pending" />
+    </div>
+  );
 };
 
 export default PendingEstimatesPage;
---
+++ src/features/my-estimates/pending/ui/tab/index.tsx
@@ -0,0 +1,48 @@
+import clsx from 'clsx';
+import Link from 'next/link';
+
+interface PendingEstimatesTabProps {
+  activeTab: 'pending' | 'received';
+}
+
+const PendingEstimatesTab = ({ activeTab = 'pending' }: PendingEstimatesTabProps) => {
+  const tabStyles = 'py-4 flex items-center justify-center cursor-pointer';
+  const activeTabStyles =
+    'border-b-2 border-solid border-black-500 text-xl font-semibold text-black-500';
+  const inactiveTabStyles = 'text-xl font-semibold text-gray-400';
+
+  const tab = {
+    pending: {
+      label: '대기 중인 견적',
+      href: '/user/my/estimates/pending',
+    },
+    received: {
+      label: '받았던 견적',
+      href: '/user/my/estimates/received',
+    },
+  };
+
+  return (
+    <div className="mx-auto w-full max-w-[1200px] pt-4">
+      <div className="flex items-center gap-8">
+        <Link
+          className={clsx(tabStyles, activeTab === 'pending' ? activeTabStyles : inactiveTabStyles)}
+          href={tab.pending.href}
+        >
+          {tab.pending.label}
+        </Link>
+        <Link
+          className={clsx(
+            tabStyles,
+            activeTab === 'received' ? activeTabStyles : inactiveTabStyles,
+          )}
+          href={tab.received.href}
+        >
+          {tab.received.label}
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PendingEstimatesTab;
