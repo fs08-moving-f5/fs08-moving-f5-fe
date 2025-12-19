bd2495ff12d76ab5e6011ad9b9a9e85068bbc27f
MinJun916 <ed125248@gmail.com>
2025-12-19 10:07:22 +0900

    chore: standardize folder names to lowercase

---
+++ src/shared/button/index.tsx
@@ -0,0 +1,6 @@
+export { default as Button } from './Button';
+export { default as LikeButton } from './LikeButton';
+export { default as ShareButton } from './ShareButton';
+export { default as Clip } from './Clip';
+export { default as Filter } from './Filter';
+export { default as CheckBox } from './CheckBox';
